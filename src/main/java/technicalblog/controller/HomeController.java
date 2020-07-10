ad167df8d784c4b37aed24ad04fcfd5017f2e2cd
Abhinav Srivatava <abhinav96fzd@gmail.com>
2020-07-11 00:58:14 +0530

    First step

---
+++ src/main/java/technicalblog/controller/HomeController.java
@@ -0,0 +1,50 @@
+package technicalblog.controller;
+import org.springframework.stereoype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import technicalblog.model.Post;
+
+
+import java.util.ArrayList;
+import java.util.Date;
+
+@Controller
+public class HomeController {
+    @RequestMapping("/")
+    public String getAllPost(Model model){
+
+        ArrayList<Post> posts = new ArrayList<>();
+
+        Post post1 = new Post();
+        post1.setTitle("Post 1");
+        post1.setBody("Body 1");
+        post1.setDate(new Date());
+
+        Post post2 = new Post();
+        post2.setTitle("Post 2");
+        post2.setBody("Body 2");
+        post2.setDate(new Date());
+
+
+        Post post3 = new Post();
+        post3.setTitle("Post 3");
+        post3.setBody("Body 3");
+        post3.setDate(new Date());
+
+        posts.add(post1);
+        posts.add(post2);
+        posts.add(post3);
+
+        model.addAttribute( s: "posts" , posts);
+
+
+
+        return "index";
+
+
+
+
+
+
+    }
+}
